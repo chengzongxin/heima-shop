28f9baca728d8d5abf6304658887bd1d42f3ae6b
chengzongxin <8876142@qq.com>
2023-08-24 01:05:55 +0800

    feat:推荐

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import CustomNavbar from './components/CustomNavbar.vue'
-import { getHomeBannerAPI, getHomeCategoryAPI } from '@/services/home'
+import { getHomeBannerAPI, getHomeCategoryAPI, getHomeHotAPI } from '@/services/home'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
 import CategoryPanel from './components/CategoryPanel.vue'
-import type { BannerItem, CategoryItem } from '../../types/home'
+import type { BannerItem, CategoryItem, HotItem } from '../../types/home'
+import { HotPanel } from './components/HotPanel.vue'
 
 const bannerList = ref<BannerItem[]>([])
-const pannelList = ref<CategoryItem[]>([])
+const pannelList = ref<HotItem[]>([])
+const hotItemList = ref<CategoryItem[]>([])
 
 const getBannerList = async () => {
   const res = await getHomeBannerAPI()
@@ -19,9 +21,16 @@ const getPannelList = async () => {
   pannelList.value = res.result
 }
 
+const getHotItemList = async () => {
+  const res = await getHomeHotAPI()
+  hotItemList.value = res.result
+  console.log('res', res)
+}
+
 onLoad(async (option) => {
   getBannerList()
   getPannelList()
+  getHotItemList()
 })
 </script>
 
@@ -30,6 +39,7 @@ onLoad(async (option) => {
     <CustomNavbar />
     <XtxSwiper :list="bannerList" />
     <CategoryPanel :list="pannelList" />
+    <HotPanel :list="hotItemList" />
   </view>
 </template>
 
