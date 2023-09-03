9cc133745ed7db8f0a363de21cc02b927e9f9dc9
chengzongxin <8876142@qq.com>
2023-09-03 13:58:48 +0800

    feat:地址管理新建、修改

--- src/pagesMember/address-form/address-form.vue
+++ src/pagesMember/address-form/address-form.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { postMemberAddressAPI } from '@/services/address'
+import {
+  postMemberAddressAPI,
+  getMemberAddressByIdAPI,
+  putMemberAddressByIdAPI,
+} from '@/services/address'
+import { onLoad } from '@dcloudio/uni-app'
 
 // 表单数据
 const form = ref({
@@ -18,6 +23,21 @@ const form = ref({
 const query = defineProps<{
   id?: string
 }>()
+
+// 获取收货地址详情数据
+const getMemberAddressByIdData = async () => {
+  // 有 id 才调用接口
+  if (query.id) {
+    // 发送请求
+    const res = await getMemberAddressByIdAPI(query.id)
+    // 把数据合并到表单中
+    Object.assign(form.value, res.result)
+  }
+}
+// 页面加载
+onLoad(() => {
+  getMemberAddressByIdData()
+})
 
 // 动态设置标题
 uni.setNavigationBarTitle({ title: query.id ? '修改地址' : '新建地址' })
@@ -38,8 +58,14 @@ const onSwitchChange: UniHelper.SwitchOnChange = (ev) => {
 
 // 提交表单
 const onSubmit = async () => {
-  // 新建地址请求
-  await postMemberAddressAPI(form.value)
+  // 判断当前页面是否有地址 id
+  if (query.id) {
+    // 修改地址请求
+    await putMemberAddressByIdAPI(query.id, form.value)
+  } else {
+    // 新建地址请求
+    await postMemberAddressAPI(form.value)
+  }
   // 成功提示
   uni.showToast({ icon: 'success', title: '添加成功' })
   // 返回上一页
@@ -51,7 +77,6 @@ const onSubmit = async () => {
 
 <template>
   <view class="content">
-    {{ form }}
     <form>
       <!-- 表单内容 -->
       <view class="form-item">
